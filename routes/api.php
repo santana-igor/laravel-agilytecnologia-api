ef9be3010d9712b9fc7e1f1dd286a9c3390bc8b3
Igor Developer <igor.amaral@iamaral.com.br>
2020-11-02 19:57:45 -0300

    Alteraçao no RouteServiceProvider para se adequar a exigencia do test phpunit

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+
+Route::group(['namespace' => 'Api'], function () {
+    Route::get('/user-timelogs', 'Reports@indexUsersTimelog');
+    Route::get('/component-metadata', 'Reports@indexComponentMetadata');
+});
+
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::group(['namespace' => 'Api'], function () {
-    Route::get('/user-timelogs', 'Reports@indexUsersTimelog');
-    Route::get('/component-metadata', 'Reports@indexComponentMetadata');
-});
